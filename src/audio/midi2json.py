ebc5d27c7d63b8dd362efdafb29f0dcefe3bc5e7
Nice Zombies <nineteendo19d0@gmail.com>
2025-10-13 21:31:48 +0200

    Write json to file

--- src/audio/midi2json.py
+++ src/audio/midi2json.py
@@ -4,9 +4,10 @@ from __future__ import annotations
 __all__: list[str] = []
 
 from argparse import ArgumentParser, Namespace
+from contextlib import redirect_stdout
 from fractions import Fraction
 
-import jsonyx
+import jsonyx as json
 from mido import MidiFile, MidiTrack  # type: ignore
 
 _NOTE_NAMES: list[str] = [
@@ -32,35 +33,50 @@ def _midi_to_note_string(midi_note: int) -> str:
 
 def _convert_track(track: MidiTrack, ticks_per_beat: int) -> None:
     total_duration: Fraction = Fraction()
+    first: bool = True
+    print("[")
     for msg in track:
         if duration := Fraction(_PPQ * msg.time, ticks_per_beat):
+            if not first:
+                print(",")
+
+            if total_duration and not total_duration % (4 * _PPQ):
+                print()
+
             total_duration += duration
             if msg.type == 'note_on' and msg.velocity:
-                jsonyx.dump({
-                    "note": None,
-                    "ticks": duration.numerator,
-                    "division": duration.denominator
-                })
+                note_str: str | None = None
             elif (
                 msg.type == 'note_on' and not msg.velocity
                 or msg.type == 'note_off'
             ):
-                note_str: str = _midi_to_note_string(msg.note)
-                jsonyx.dump({
-                    "note": note_str,
-                    "ticks": duration.numerator,
-                    "division": duration.denominator,
-                })
+                note_str = _midi_to_note_string(msg.note)
+            else:
+                continue
 
-            if not total_duration % 64:
-                print()
+            first = False
+            division: int = duration.denominator
+            print(end=4 * " ")
+            json.dump({
+                "note": note_str,
+                "ticks": duration.numerator,
+                **({"division": division} if division > 1 else {})
+            }, end="")
+
+    print()
+    print(end="]")
 
 
 def _main() -> None:
     video_number: int = _parse_args().video_number
     midi: MidiFile = MidiFile(f'videos/{video_number}/audio/simple.mid')
-    for track in midi.tracks:
-        _convert_track(track, midi.ticks_per_beat)
+    tracks: list[MidiTrack] = midi.tracks
+    if len(tracks) != 1:
+        raise ValueError("Not exactly one track")
+
+    json_filename: str = f'videos/{video_number}/audio/simple.json'
+    with open(json_filename, 'w', encoding='utf-8') as fp, redirect_stdout(fp):
+        _convert_track(tracks[0], midi.ticks_per_beat)
 
 
 if __name__ == "__main__":
