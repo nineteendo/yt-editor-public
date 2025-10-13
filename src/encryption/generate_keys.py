fbff9629f725007bb28b52bf9461ca6fc2257174
Nice Zombies <nineteendo19d0@gmail.com>
2025-10-13 21:37:28 +0200

    Create missing directories

--- src/encryption/generate_keys.py
+++ src/encryption/generate_keys.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 
 __all__: list[str] = []
 
+from os import makedirs
+
 from cryptography.hazmat.primitives.asymmetric.rsa import (
     RSAPrivateKey, RSAPublicKey, generate_private_key
 )
@@ -14,6 +16,7 @@ from cryptography.hazmat.primitives.serialization import (
 def _main() -> None:
     private_key: RSAPrivateKey = generate_private_key(65537, 4096)
     public_key: RSAPublicKey = private_key.public_key()
+    makedirs("auth", exist_ok=True)
     with open("auth/private.pem", "wb") as fp:
         fp.write(private_key.private_bytes(
             Encoding.PEM, PrivateFormat.PKCS8, NoEncryption()
